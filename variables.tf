b3edc84caad410d430b59ff9841026fd534d9fc8
Tim Crall <timcrall@gmail.com>
2019-01-15 11:58:12 -0800

    Changes ssh username and password variable name

--- variables.tf
+++ variables.tf
@@ -2,8 +2,8 @@ variable "vsphere_password" {
   description = "vsphere password for the specified vsphere username"
 }
 
-variable "terraform_password" {
-  description = "password for the terraform account on the specified disk image"
+variable "ssh_password" {
+  description = "password to use for ssh"
 }
 
 variable "ucp_admin_password" {
