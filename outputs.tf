da4491a64684c529b512fa5c27b31f841fbd8295
Craig Snow <csnow@Craigs-MacBook-Pro-2.local>
2019-04-10 14:19:58 -0700

    new stuff

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "dtr_url" {
-  value = "${module.dockeree-cluster.dtr_url}"
+ value = "${module.dockeree-cluster.dtr_url}"
 }
 
 output "ucp_url" {
@@ -10,14 +10,6 @@ output "manager_ips" {
   value = "${module.dockeree-cluster.manager_ips}"
 }
 
-output "worker_ips" {
-  value = "${module.dockeree-cluster.worker_ips}"
-}
-
 output "dtr_ips" {
   value = "${module.dockeree-cluster.dtr_ips}"
-}
-
-output "minio_ip" {
-  value = "${module.dockeree-cluster.minio_ip}"
 }
