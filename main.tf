011edfe1d0016e8ac9f86cb6e2cfbd8301c10506
Craig Snow <csnow@Craigs-MacBook-Pro-2.local>
2019-05-17 17:01:45 -0700

    new version of UCP

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "dockeree-cluster" {
   source                      = "git@github.com:IGNW/terraform-vsphere-dockeree?ref=vesta"
 
-  environment                 = "csnow"
+  environment                 = "centos"
   vsphere_server              = "10.254.252.5"
   vsphere_datacenter          = "POC-Lab"
-  dtr_fqdn                    = "dtr-csnow.ignw.local"
-  ucp_fqdn                    = "ucp-csnow.ignw.local"
+  dtr_fqdn                    = "dtr-centos.ignw.local"
+  ucp_fqdn                    = "ucp-centos.ignw.local"
 
   manager_vsphere_datastore   = "10.254.252.23_Datastore"
   manager_vsphere_cluster     = "POC"
@@ -24,13 +24,13 @@ module "dockeree-cluster" {
   worker_b_vsphere_network    = "ignw-poc|vesta-devops|servers"
   worker_b_vm_template	      = "centos7_template_dockeree_UCP316"
 
-  vsphere_folder              = "dockeree-csnow"
+  vsphere_folder              = "dockeree-centos"
   domain                      = "ignw.io"
   manager_node_count          = "3"
   worker_a_node_count         = "2"
   worker_b_node_count         = "2"
   dtr_node_count              = "3"
-  dtr_nfs_url                 = "nfs://10.50.2.233/data"
+  dtr_nfs_url                 = "nfs://10.50.2.233/dtr-centos"
   vsphere_user                = "administrator@vsphere.local"
   vsphere_password            = "Ignw!098"
   ssh_username                = "root"
@@ -42,12 +42,12 @@ module "dockeree-cluster" {
   scsi_type                   = "pvscsi"
   script_path                 = "/tmp"
   dockeree_license	      = ""
-  ucp_version                = "3.1.6"
-  dtr_version                = "2.6.5"
+  ucp_version                = "3.1.7"
+  dtr_version                = "2.6.6"
   consul_version             = "1.4.4"
 
   load_balancer_count        = "2"
-  load_balancer_ips          = ["10.50.2.70"]
+  load_balancer_ips          = ["10.50.2.71"]
   load_balancer_script_path  = "/tmp"
   load_balancer_username     = "root"
   load_balancer_password     = "changeme1"
